ff7b0b5d05df397247bdf9f0c11bd877038e7cdc
Barry Jay <barry.jay@barry.jay.macbook>
2019-01-07 15:10:47 +1100

    restored paths to work with .bondi and prelude

--- src/system.ml
+++ src/system.ml
@@ -1,7 +1,8 @@
 (* BONDI version must be moved to ../VERSION *)
 
 let version = "2.09" 
-let standard_library_default = "~/.bondi"
+let standard_library_default =
+     Printf.sprintf "%s/.bondi" @@ Unix.getenv "HOME"
 let standard_library =
   try
     Sys.getenv "BONDI_LIB_DIR"
